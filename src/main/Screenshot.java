437564c509e09ba57c057b5427fe634f213a9918
varalakshmig_grhombus <varalakshmig@grhombus.com>
2020-03-07 15:00:31 +0530

    added test class

--- src/main/Screenshot.java
+++ src/main/Screenshot.java
@@ -11,6 +11,10 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Screenshot 
 {
+	void Test()
+	{
+		System.out.println("Test");
+	}
 	public static void main(String[] args) throws IOException 
 	{
 		System.setProperty("webdriver.chrome.driver", "C:\\chromedriver\\chromedriver.exe");
